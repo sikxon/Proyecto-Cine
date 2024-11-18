516be3cac05582800a9d359298c6ef8ef9d150b4
sikxon <sikxongian@gmail.com>
2024-11-18 14:49:17 -0300

    par de cosa ma

--- auth/login.php
+++ auth/login.php
@@ -1,5 +1,6 @@
 <?php
 require_once '../db_connection.php';
+require_once '../config.php';
 session_start();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
--- auth/register.php
+++ auth/register.php
@@ -1,6 +1,6 @@
 <?php
 require_once '../db_connection.php';
-
+require_once '../config.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $nombre = $_POST['nombre'];
     $apellido = $_POST['apellido'];
